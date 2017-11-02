e2b46b4a5c619ab653b4164fe7cbb7590cb11788
Sarangi90 <coolestsaa@gmail.com>
2017-11-02 16:06:22 +0100

    transform implemented by changing uarm_control.cpp|build through|tested

--- ras_project_tf_listener/src/tf_listener.cpp
+++ ras_project_tf_listener/src/tf_listener.cpp
@@ -1,13 +1,24 @@
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
+#include <std_msgs/Bool.h>
 #include <geometry_msgs/PointStamped.h>
 #include <ras_project_uarm/MoveArmCartesian.h>
+
+#define TRUE 1
+
+static bool objectDetected;
+
+void chatterCallback(const std_msgs::Bool::ConstPtr& msg)
+{
+  objectDetected = msg->data;
+}
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "tf_listener");
 
   ros::NodeHandle node;
   ros::ServiceClient client = node.serviceClient<ras_project_uarm::MoveArmCartesian>("/uarm/moveToPose");
+  ros::Subscriber sub = node.subscribe("object_detection", 1, chatterCallback);
   ras_project_uarm::MoveArmCartesian srv;
   tf::TransformListener listener;
 
@@ -15,30 +26,36 @@ int main(int argc, char** argv){
   while (node.ok())
   {
     tf::StampedTransform transform;
-    try
+    if(objectDetected == TRUE && srv.response.error != TRUE)
     {
-      listener.lookupTransform("uarm_base", "object",
-                               ros::Time(0), transform);
-    }
-    catch (tf::TransformException &ex)
-    {
-      ROS_ERROR("%s",ex.what());
-      ros::Duration(1.0).sleep();
-      continue;
-    }
+        try
+        {
+            listener.lookupTransform("uarm_base", "object", ros::Time(0), transform);
+        }
+        catch (tf::TransformException &ex)
+        {
+            ROS_ERROR("%s",ex.what());
+            ros::Duration(1.0).sleep();
+            continue;
+        }
 
-    geometry_msgs::PointStamped uArmObjPos_msg;
-    uArmObjPos_msg.point.x = transform.getOrigin().x();
-    uArmObjPos_msg.point.y = transform.getOrigin().y();
-    uArmObjPos_msg.point.z = transform.getOrigin().z();
-    uArmObjPos_msg.header.frame_id = "uArm_base";
-    uArmObjPos_msg.header.stamp = ros::Time();
-    ROS_INFO("object -----> uArm_base: (%.2f, %.2f, %.2f) at time %.2f",
-    uArmObjPos_msg.point.x, uArmObjPos_msg.point.y, uArmObjPos_msg.point.z, uArmObjPos_msg.header.stamp.toSec());
-    srv.request.point = uArmObjPos_msg;
-    if(client.call(srv))
+     geometry_msgs::PointStamped uArmObjPos_msg;
+     uArmObjPos_msg.point.x = transform.getOrigin().x();
+     uArmObjPos_msg.point.y = transform.getOrigin().y();
+     uArmObjPos_msg.point.z = transform.getOrigin().z();
+     uArmObjPos_msg.header.frame_id = "uArm_base";
+     uArmObjPos_msg.header.stamp = ros::Time();
+     ROS_INFO("object -----> uArm_base: (%.2f, %.2f, %.2f) at time %.2f",
+     uArmObjPos_msg.point.x, uArmObjPos_msg.point.y, uArmObjPos_msg.point.z, uArmObjPos_msg.header.stamp.toSec());
+     srv.request.point = uArmObjPos_msg;
+     if(client.call(srv))
+     {
         ROS_INFO("%d",srv.response.error);
+        objectDetected = !TRUE;
+     }
+    }
 
+    ros::spinOnce();
     rate.sleep();
   }
   return 0;
