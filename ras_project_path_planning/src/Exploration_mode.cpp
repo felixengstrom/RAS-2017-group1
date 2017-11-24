1dcc03dbf9c5d89b35ca9b7b041e5daae2c4815f
Harimus <dan.lillrank@gmail.com>
2017-11-24 16:46:46 +0100

    HardCoded part removed from Occupancygrid and Path Planning. Now can take things dynamically

--- ras_project_path_planning/src/Exploration_mode.cpp
+++ ras_project_path_planning/src/Exploration_mode.cpp
@@ -17,16 +17,49 @@
 #include <map>
 #include <algorithm>
 
+
+
 class Exploration
 {
+	private:
+		ros::NodeHandle n;
+	
+		//MAP Subscribers
+		ros::Subscriber OG_sub; // OccupancyGrid Subscribe
+		ros::Subscriber Csp_sub; //Configuration space Subscribe
+		//Current Position Subscriber
+		ros::Subscriber curr_sub;
+		
+		//--------------//		
+		//Path Publisher
+		ros::Publisher Dest_pub; //Destination publisher
+		//--------------------//
+		//State Variable  
+		ros::Time t_update;
+		nav_msgs::OccupancyGrid Exp; //EXPERIENCE!
+		bool Exp_initialized;
+		//-------------------------//
+		
+		//Private methods
+		void OGCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg);
+		void CspCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg);
+		void CurrCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
+		
+	public:
 
+		Exploration() : Exp_initialized(false)
+		{
+			//Initialize
+			n = ros::NodeHandle();
+			OG_sub = n.subscribe("/maze_OccupancyGrid",10,&Exploration::OGCallback,this);
+			Csp_sub = n.subscribe("maze_CSpace",10,&Exploration::CspCallback,this);		
+			curr_sub = n.subscribe("/robot/pose",10,&Exploration::CurrCallback,this);
+			
+		}
 
+		void loop_function();
 
 };
 
-
-
-
-
-
+Exploration::OGCallback(const nav_msgs::OccupancyGrid::ConstPtr&
 
